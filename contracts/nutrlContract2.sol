ea49cd968270a2f481a5efcf6c8dbfdfb8bd98e9
Andrei Homentcovschi <ahomentc@gmail.com>
2018-09-02 18:45:40 -0700

    v3

--- contracts/nutrlContract2.sol
+++ contracts/nutrlContract2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.20;
 contract ContractManager
 {       
     // hash of a generated password gives an array of contract addresses for that password
-    mapping(uint => address[] contracts) public ContractLists;
+    mapping(uint => address[]) public ContractLists;
 
     /// <summary>
     /// - Create a new contract and get the address.
@@ -12,8 +12,8 @@ contract ContractManager
     /// </summary>
     function createNewContract(uint password_hash, string name, string content)
     {
-        IndividualContract new_contract = new IndividualContract(name, content);
-        new_contract.participants.push(msg.sender);
+        IndividualContract new_contract = new IndividualContract();
+        new_contract.init(name,content,msg.sender);
         appendToContractLists(password_hash, new_contract);
     }
 
@@ -28,9 +28,9 @@ contract ContractManager
         ContractLists[password_hash].push(contract_address);
     }
 
-    function modifyContract(string content, string name, contract individual_contract)
+    function modifyContract(string content, string name, IndividualContract individual_contract)
     {
-        individual_contract.createProposal(content, name)
+        individual_contract.createProposal(content, name);
     }
 
 }
@@ -41,15 +41,23 @@ contract IndividualContract
     string content;
     address[] participants;
     address[] proposals;
+    
+    function init(string set_name, string set_content, address set_user)
+    {
+        name = set_name;
+        content = set_content;
+        participants.push(set_user);
+    }
 
     /// <summary>
     /// - Creates a new contract proposal
     /// </summary>
     /// <param name="new_content">The new content that is being proposed</param>
-    function createProposal(string new_content, string new_name = name)
+    function createProposal(string new_content, string new_name)
     {
-        contractProposal new_proposal = new contractProposal(new_name, content)
-        proposals.push(new_proposal)
+        contractProposal new_proposal = new contractProposal();
+        new_proposal.init(new_name, new_content);
+        proposals.push(new_proposal);
     }
 
     /// <summary>
@@ -59,8 +67,8 @@ contract IndividualContract
     /// <param name="proposal">The proposal that is being accepted</param>
     function acceptProposal(contractProposal proposal)
     {
-        name = proposal.name;
-        content = proposal.content;
+        name = proposal.getName();
+        content = proposal.getContent();
     }
 
     function voteToAcceptProposal(address user, contractProposal proposal)
@@ -71,13 +79,13 @@ contract IndividualContract
         // if the user isn't in the yes array, add the user 
         if(!proposal.userInYesVotes(user))
         {
-            proposal.addUserToYesVotes(user)
+            proposal.addUserToYesVotes(user);
         }
 
         // if the yes array contains everyone, accept it
-        if(proposal.getNumYesVotes == participant.length)
+        if(proposal.getNumYesVotes() == participants.length)
         {
-            acceptProposal(proposal)
+            acceptProposal(proposal);
         }
 
     }
@@ -98,9 +106,9 @@ contract IndividualContract
     /// - Check if user in particpants list
     /// </summary>
     /// <param name="_user">The user's address that is being checked</param>
-    function _userInPartipants(address _user)
+    function _userInPartipants(address _user) returns (bool)
     {
-        for( int i=0; i < particpants.length; i++)
+        for( uint i=0; i < participants.length; i++)
         {
             if(participants[i] == _user)
             {
@@ -117,12 +125,28 @@ contract contractProposal
     string content;
 
     address[] yesVotes;
-
-    function userInYesVotes(address _user)
+    
+    function init(string set_name, string new_content)
     {
-        for( int i=0; i < yesVotes.length; i++)
+        name = set_name;
+        content = new_content;
+    }
+    
+    function getName() returns (string)
+    {
+        return name;
+    }
+    
+    function getContent() returns (string)
+    {
+        return content;
+    }
+
+    function userInYesVotes(address _user) returns (bool)
+    {
+        for( uint i=0; i < yesVotes.length; i++)
         {
-            if(participants[i] == _user)
+            if(yesVotes[i] == _user)
             {
                 return true;
             }
@@ -132,10 +156,10 @@ contract contractProposal
 
     function addUserToYesVotes(address _user)
     {
-        yesVotes.push(_user)
+        yesVotes.push(_user);
     }
 
-    function getNumYesVotes()
+    function getNumYesVotes() returns (uint)
     {
         return yesVotes.length;
     }
